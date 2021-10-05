ba77c2ab2576199557e8f67fbf417f5ba11424bf
Marcos de Gois Borges <61516804+goisborges@users.noreply.github.com>
2021-10-05 15:11:56 -0400

    Add project files.

---
+++ Models/Match.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MortoSweepstakes.Models
+{
+    public class Match
+    {
+        //model for the matches
+        public int MatchId { get; set; }
+
+        public string Team1 { get; set; }
+
+        public string Team2 { get; set; }
+
+        public int ScoreTeam1 { get; set; }
+
+        public int ScoreTeam2 { get; set; }
+
+        public DateTime MatchDateTime { get; set; }
+
+        //Reference to child Bet model
+        public List<Bet> Bets { get; set; }
+
+    }
+}
