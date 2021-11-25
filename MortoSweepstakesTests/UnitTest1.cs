74b514607bb286a8d8c602e4e4aeca7a565aa774
Marcos de Gois Borges <61516804+goisborges@users.noreply.github.com>
2021-11-24 20:44:14 -0500

    Test project and class created.

---
+++ MortoSweepstakesTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MortoSweepstakesTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
